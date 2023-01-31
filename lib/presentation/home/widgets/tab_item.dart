d786c34b4613cff3d94f9040431f2b481759c9f1
Davor Stajcer <Davor Stajcer>
2023-01-31 17:54:25 +0100

    Tab hover mobile fix

--- lib/presentation/home/widgets/tab_item.dart
+++ lib/presentation/home/widgets/tab_item.dart
@@ -5,6 +5,7 @@ import 'package:misli_os_app/domain/models/home_tab.dart';
 import 'package:misli_os_app/domain/providers/tab_hovered_provider/tab_hovered_provider.dart';
 import 'package:misli_os_app/domain/providers/tab_picked_provider/tab_index_provider.dart';
 import 'package:misli_os_app/domain/providers/tab_state_provider/tab_state_provider.dart';
+import 'package:misli_os_app/presentation/common/values/is_web_mobile.dart';
 import 'package:misli_os_app/presentation/home/widgets/tab_hovered.dart';
 import 'package:misli_os_app/presentation/home/widgets/tab_selected.dart';
 import 'package:misli_os_app/presentation/home/widgets/tab_unselected.dart';
@@ -21,22 +22,26 @@ class Tabitem extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final tabState = ref.watch(tabStateProvider(tabIndex));
+    final tabWidget = GestureDetector(
+      onTap: () {
+        ref.read(tabIndexProvider.notifier).state = tabIndex;
+        GoRouter.of(context).go('/tabs/$tabIndex');
+      },
+      child: tabState.when(
+        unselected: () => TabUnselected(homeTab),
+        selected: () => TabSelected(homeTab),
+        hovered: () => TabHovered(homeTab),
+      ),
+    );
+    if (kIsWebMobile) {
+      return tabWidget;
+    }
     return MouseRegion(
       onHover: (event) =>
           ref.read(tabHoveredProvider.notifier).onTabHovered(tabIndex),
       onExit: (event) =>
           ref.read(tabHoveredProvider.notifier).onTabHovered(null),
-      child: GestureDetector(
-        onTap: () {
-          ref.read(tabIndexProvider.notifier).state = tabIndex;
-          GoRouter.of(context).go('/tabs/$tabIndex');
-        },
-        child: tabState.when(
-          unselected: () => TabUnselected(homeTab),
-          selected: () => TabSelected(homeTab),
-          hovered: () => TabHovered(homeTab),
-        ),
-      ),
+      child: tabWidget,
     );
   }
 }
--- lib/presentation/home/widgets/tab_item_small.dart
+++ lib/presentation/home/widgets/tab_item_small.dart
@@ -4,6 +4,7 @@ import 'package:misli_os_app/domain/models/home_tab.dart';
 import 'package:misli_os_app/domain/providers/tab_hovered_provider/tab_hovered_provider.dart';
 import 'package:misli_os_app/domain/providers/tab_picked_provider/tab_index_provider.dart';
 import 'package:misli_os_app/domain/providers/tab_state_provider/tab_state_provider.dart';
+import 'package:misli_os_app/presentation/common/values/is_web_mobile.dart';
 import 'package:misli_os_app/presentation/home/widgets/tab_hovered.dart';
 import 'package:misli_os_app/presentation/home/widgets/tab_hovered_small.dart';
 import 'package:misli_os_app/presentation/home/widgets/tab_selected.dart';
@@ -22,22 +23,27 @@ class TabitemSmall extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final tabState = ref.watch(tabStateProvider(tabIndex));
+    final tabWidget = GestureDetector(
+      onTap: () {
+        ref.read(tabIndexProvider.notifier).state = tabIndex;
+        Scaffold.of(context).closeDrawer();
+      },
+      child: tabState.when(
+        unselected: () => TabUnselectedSmall(homeTab),
+        selected: () => TabSelectedSmall(homeTab),
+        hovered: () => TabHoveredSmall(homeTab),
+      ),
+    );
+
+    if (kIsWebMobile) {
+      return tabWidget;
+    }
     return MouseRegion(
       onHover: (event) =>
           ref.read(tabHoveredProvider.notifier).onTabHovered(tabIndex),
       onExit: (event) =>
           ref.read(tabHoveredProvider.notifier).onTabHovered(null),
-      child: GestureDetector(
-        onTap: () {
-          ref.read(tabIndexProvider.notifier).state = tabIndex;
-          Scaffold.of(context).closeDrawer();
-        },
-        child: tabState.when(
-          unselected: () => TabUnselectedSmall(homeTab),
-          selected: () => TabSelectedSmall(homeTab),
-          hovered: () => TabHoveredSmall(homeTab),
-        ),
-      ),
+      child: tabWidget,
     );
   }
 }
